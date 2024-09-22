0c6c70367f2b34c35d37bd21d7297175e7fa4e12
bryanfks-dev <bryanfernandoks11@gmail.com>
2024-09-22 12:53:16 +0700

    minor change: update register form data type

--- lib/domain/entities/forms.dart
+++ lib/domain/entities/forms.dart
@@ -18,21 +18,21 @@ class LoginFormData {
 /// RegisterFormData is a class that holds the data that is needed to register a user.
 ///
 /// [username] is the username of the user.
+/// [phoneNumber] is the phone number of the user.
 /// [password] is the password of the user.
 /// [confirmPassword] is the confirmation password of the user.
-/// [phoneNumber] is the phone number of the user.
 class RegisterFormData {
   /// The username of the user.
   String username;
+
+  /// The phone number of the user.
+  String phoneNumber;
 
   /// The password of the user.
   String password;
 
   /// The confirmation password of the user.
   String confirmPassword;
-
-  /// The phone number of the user.
-  String phoneNumber;
 
   RegisterFormData({
     required this.username,
