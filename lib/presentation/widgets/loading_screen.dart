75a1ceb91d09a19ea499c9c69ef9a2e8d3e30f4a
bryanfks-dev <bryanfernandoks11@gmail.com>
2024-12-21 13:54:50 +0700

    update loading screen

--- lib/presentation/widgets/loading_screen.dart
+++ lib/presentation/widgets/loading_screen.dart
@@ -1,4 +1,3 @@
-import 'package:courtly/core/config/app_color_extension.dart';
 import 'package:flutter/material.dart';
 
 /// [LoadingScreen] is a widget that displays a loading screen.
@@ -10,33 +9,15 @@ class LoadingScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    /// [colorExt] is the color extension for the application.
-    final AppColorsExtension colorExt =
-        Theme.of(context).extension<AppColorsExtension>()!;
-
-    return Container(
-        color: Colors.black.withOpacity(0.5),
-        child: Center(
-          child: Container(
-            padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 20),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              color: colorExt.background,
-            ),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                const CircularProgressIndicator(),
-                const SizedBox(
-                  height: 10,
-                ),
-                Text(
-                  label ?? "Loading...",
-                  style: const TextStyle(fontSize: 14),
-                )
-              ],
-            ),
-          ),
-        ));
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          CircularProgressIndicator(),
+          SizedBox(height: 10),
+          Text(label ?? 'Loading...'),
+        ],
+      ),
+    );
   }
 }
